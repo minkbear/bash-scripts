ea3a90d0e26ab4b0e18341b09c0efd67ec92de0e
Minkbear <minkbear@gmail.com>
2019-12-30 16:05:09 +0700

    add configSwapfile

--- install/amp_debian.sh
+++ install/amp_debian.sh
@@ -180,6 +180,20 @@ restartApache() {
 	sudo service apache2 restart
 }
 
+configSwapfile() {
+	if [ -f /swapfile ]; then
+    	echo "Swap exists."
+	else
+		fallocate -l 1G /swapfile
+		chmod 600 /swapfile
+		mkswap /swapfile
+		swapon /swapfile
+		echo "/swapfile none swap sw 0 0" >> /etc/fstab
+		echo "vm.swappiness=30" >> /etc/sysctl.conf
+		echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
+	fi
+}
+
 # RUN
 update
 disablePasswordSSH
@@ -195,6 +209,7 @@ installPHPMyAdmin
 enableMods
 setPermissions
 restartApache
+configSwapfile
 
 echo -e "\n${Green} SUCCESS! MySQL password is: ${PASS_MYSQL_ROOT} ${Color_Off}"
 echo ${PASS_MYSQL_ROOT} > /root/.mysql_root_pass
